19b6b0ccc8bef22996168e3cb8c7d4f52bc75380
Lucas Rocha <lukasrocha0402@gmail.com>
2023-01-27 21:32:33 -0300

    Adicionando a resolução do primeiro desafio codewars

---
+++ exerciopoo/src/main/java/BeginnnerSeries2.java
@@ -0,0 +1,9 @@
+public class BeginnnerSeries2 {
+    /*
+    O relógio mostra h horas, m minutos e s segundos depois da meia-noite.
+    Sua tarefa é escrever uma função que retorne a hora desde a meia-noite em milissegundos.
+     */
+    public static int Pasta(int h, int m, int s){
+        return h * 3600000 + m * 60000 + s * 1000;
+    }
+}
